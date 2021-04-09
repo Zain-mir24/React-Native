5862409863f5d83756832263ab303be40081761d
Zain Mir <zainnaeemk10@gmail.com>
2021-04-09 10:40:27 +0500

    labsessional

--- App.js
+++ App.js
@@ -4,68 +4,88 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
 
-  const [getText, setText] = useState("")
-  const [randomnumber,changenumber]=useState(Math.trunc(Math.random()*99))
+  // const [getText, setText] = useState("")
+  // const [randomnumber,changenumber]=useState(Math.trunc(Math.random()*99))
 
-  const buttonClick = (txt) => {
-    // alert(txt)
-    setText(getText + txt)
-  }
+  // const buttonClick = (txt) => {
+  //   // alert(txt)
+  //   setText(getText + txt)
+  // }
 
-  const butnlick=(text)=>{
+  // const butnlick=(text)=>{
     
-    if(text=={randomnumber}){
-      console.log('correct')
-    }
-    else{
-      console.log('fail')
-    }
-  }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your p!</Text>
-      <Header fullname="Zain mir" />
-     <View>{randomnumber}</View>
-      <Text style={{ fontSize: 50 }}>{getText}</Text>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={{ width: 50 }}>
-          <Button title="1" color="green" onPress={buttonClick.bind(this,1)} />        
-        <Button title="2" color="red" onPress={buttonClick.bind(this,2 )} />
-        <Button title="3" color="yellow" onPress={buttonClick.bind(this, 3)} />
-        <Button title="4" color="orange" onPress={buttonClick.bind(this,4)} />
-        <Button title="5" color="pink" onPress={buttonClick.bind(this,5 )} />
-        <Button title="6" color="purple" onPress={buttonClick.bind(this,6)} />
-        <Button title="7" color="purple" onPress={buttonClick.bind(this,7)} />
-        <Button title="8" color="purple" onPress={buttonClick.bind(this,8)} />
-        <Button title="9" color="purple" onPress={buttonClick.bind(this,9)} />
-        <Button title="guess"color="purple" onPress={butnlick.bind(this,{setText})} />
+  //   if(text=={randomnumber}){
+  //     console.log('correct')
+  //   }
+  //   else{
+  //     console.log('fail')
+  //   }
+  // }
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.js to start working on your p!</Text>
+  //     <Header fullname="Zain mir" />
+  //    <View>{randomnumber}</View>
+  //     <Text style={{ fontSize: 50 }}>{getText}</Text>
+  //     <View style={{ flexDirection: 'row' }}>
+  //       <View style={{ width: 50 }}>
+  //         <Button title="1" color="green" onPress={buttonClick.bind(this,1)} />        
+  //       <Button title="2" color="red" onPress={buttonClick.bind(this,2 )} />
+  //       <Button title="3" color="yellow" onPress={buttonClick.bind(this, 3)} />
+  //       <Button title="4" color="orange" onPress={buttonClick.bind(this,4)} />
+  //       <Button title="5" color="pink" onPress={buttonClick.bind(this,5 )} />
+  //       <Button title="6" color="purple" onPress={buttonClick.bind(this,6)} />
+  //       <Button title="7" color="purple" onPress={buttonClick.bind(this,7)} />
+  //       <Button title="8" color="purple" onPress={buttonClick.bind(this,8)} />
+  //       <Button title="9" color="purple" onPress={buttonClick.bind(this,9)} />
+  //       <Button title="guess"color="purple" onPress={butnlick.bind(this,{setText})} />
         
-        </View>
-      </View>
-      <StatusBar style="auto" />  
-    </View>
-  );
-}
-
-const Header = (props) => {
-  return (
-    <View>
-      <Text style={headerStyles.text}>Name: {props.fullname}</Text>
-    </View>
-  )
-}
-
-const headerStyles = StyleSheet.create({
-  text: {
-    fontSize: 20
+  //       </View>
+  //     </View>
+  //     <StatusBar style="auto" />  
+  //   </View>
+  // );
+  class MyInfo extends React.Component {
+    render() {
+      return <div><h4>Name: </h4><h4>RegNo: </h4></div>
+    }
   }
-})
+  
+  class App extends React.Component {
+    render() {
+      const div = (
+        <div>
+          <MyInfo name="Sample Name" regno="Sample RegNo" />
+          <h3>Counter: 0</h3>
+          <button>Press Me to Increment Counter</button>
+        </div>
+      )
+      return div;
+    }
+  }
+  
+  export default App;
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const Header = (props) => {
+//   return (
+//     <View>
+//       <Text style={headerStyles.text}>Name: {props.fullname}</Text>
+//     </View>
+//   )
+// }
+
+// const headerStyles = StyleSheet.create({
+//   text: {
+//     fontSize: 20
+//   }
+// })
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
